ce24a1e09c547b21f90eacbca6b1991c0362001c
pmgq91 <62215568+pmgq91@users.noreply.github.com>
2023-07-20 16:01:57 -0600

    Create crontab_error_to_pendientes.py

---
+++ crontab_error_to_pendientes.py
@@ -0,0 +1,18 @@
+import gspread
+import os
+
+def f_o_tab_error(p_path_service_account):
+    o_gs_service_account = gspread.service_account(filename = p_path_service_account)
+    o_spreadsheet = o_gs_service_account.open_by_key('1JVvnOiuLusdCHMZcvHRVzbKUMrTp4QpuzGbgPV9jvck')
+    o_spreadsheet_errors = o_gs_service_account.open_by_key('12FmsjHWh0fLyuXTqHc_wHqT8uP4tNNVe5z6-hGBbUTY')
+    o_tab_error = o_spreadsheet_errors.get_worksheet_by_id(1054369469)
+    l_error_values = o_tab_error.col_values(1)
+    v_row_error_entry = len(l_error_values) + 1
+    return o_tab_error, v_row_error_entry
+
+def f_write_error(p_row_error_entry, p_error_message):
+    try:
+        v_script_name = os.path.basename(__file__)
+    except:
+        v_script_name = '??jupyter_nb??'
+    o_tab_error.update('A' + str(p_row_error_entry), v_script_name + ': ' + p_error_message)
