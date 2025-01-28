9e0a24e9abdf3d4c723d4cb1eddfe27299b524e8
pmgq91 <62215568+pmgq91@users.noreply.github.com>
2025-01-28 12:58:32 -0500

    formatting changes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(    
-    name='pmgq',
-    url='https://github.com/pmgq91/prod_packages',
-    author='Pedro Marvan',
-    author_email='pmgq@hotmail.com',
-    packages=['pmgq'],
-    version='0.1',
-    license='PMGQ',
-    description='Modules that I actually use'
+    name = 'pmgq',
+    url = 'https://github.com/pmgq91/prod_packages',
+    author = 'Pedro Marvan',
+    author_email = 'pmgq@hotmail.com',
+    packages = ['pmgq'],
+    version = '0.1',
+    license = 'PMGQ',
+    description = 'Modules that I actually use'
 )
