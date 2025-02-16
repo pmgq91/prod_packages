13054c64524776cdebc862c308a0e5a787f91cd0
pmgq91 <62215568+pmgq91@users.noreply.github.com>
2025-02-16 13:14:29 -0600

    Update crontab_error_to_pendientes.py

--- pmgq/crontab_error_to_pendientes.py
+++ pmgq/crontab_error_to_pendientes.py
@@ -3,15 +3,14 @@ import os
 
 def f_o_tab_error(p_path_service_account):
     o_gs_service_account = gspread.service_account(filename = p_path_service_account)
-    o_spreadsheet = o_gs_service_account.open_by_key('1JVvnOiuLusdCHMZcvHRVzbKUMrTp4QpuzGbgPV9jvck')
     o_spreadsheet_errors = o_gs_service_account.open_by_key('12FmsjHWh0fLyuXTqHc_wHqT8uP4tNNVe5z6-hGBbUTY')
     o_tab_error = o_spreadsheet_errors.get_worksheet_by_id(1054369469)
-    l_error_values = o_tab_error.col_values(1)
-    v_row_error_entry = len(l_error_values) + 1
-    return o_tab_error, v_row_error_entry
+    return o_tab_error
 
-def f_write_error(p_o_tab_error, p_row_error_entry, p_script_and_error_message):
-    p_o_tab_error.update('A' + str(p_row_error_entry), p_script_and_error_message)
+def f_write_error(p_o_tab_error, p_script_and_error_message):
+    l_error_values = p_o_tab_error.col_values(1)
+    v_row_error_entry = len(l_error_values) + 1    
+    p_o_tab_error.update('A' + str(v_row_error_entry), p_script_and_error_message)
 
 #USAGE:
 '''
@@ -19,11 +18,10 @@ from pmgq import crontab_error_to_pendientes as lib_error
 
 v_path_service_account = '{{path}}'
 
-o_tab_error, v_row_error_entry = lib_error.f_o_tab_error(p_path_service_account = v_path_service_account)
+o_tab_error = lib_error.f_o_tab_error(p_path_service_account = v_path_service_account)
 
 lib_error.f_write_error(
     p_o_tab_error = o_tab_error,
-    p_row_error_entry = v_row_error_entry,
     p_script_and_error_message = 'test: hola'
 )
 '''
